2698cf52b10e526929849f839742747acdc2a8cc
Racher <kty06yzlm@gmail.com>
2024-08-27 20:27:45 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'YourName'
+description 'A custom hair toggle command'
+version '1.0.0'
+
+-- Client scripts
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+-- Server scripts (eğer gerekli ise)
+server_scripts {
+    'server.lua'
+}
